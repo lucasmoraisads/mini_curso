2a8580e75b72b2b3b4399d43496ec235669feeda
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-10-31 06:52:15 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
-let idade = 90 // number
-let nome = "Lucas" // string
+let idade = 20 // number
+let nome = "Pedro" // string
 let logago = true // tipo boolean
 
+let lista = ["farinha", "fermento", "chocolate", "Ovos"]
 
-
+for (let item of lista){
+    console.log("- ingredientes: " + item)
+}
